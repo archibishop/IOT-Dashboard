d94919c96f00d4112e149453907dd868bb9ab6aa
Wagubi Brian <36948493+archibishop@users.noreply.github.com>
2019-08-14 11:39:59 +0300

    Generate PDF, add loader and refactor code. (#7)
    - Generate Pdf for the visualisations
    - Add loading gif before graph data is returned
    - Refactor code.

--- readings/urls.py
+++ readings/urls.py
@@ -3,9 +3,12 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path('home',  cache_page(60*15)(views.Home.as_view()), name='dashboard'),
+    path('home',  (views.Home.as_view()), name='dashboard'),
     path('temp', views.Temperature.as_view(), name='temp'),
     path('mositure', views.Moisture.as_view(), name='moisture'),
     path('pressure', views.Pressure.as_view(), name='pressure'),
     path('readings', views.get_readings, name='readings'),
+    path('pdf/<int:page>', views.PDF.as_view(), name='pdf'),
+    path('generate-pdf/<int:page>', views.GeneratePdf.as_view(),
+         name='generate-pdf'),        
 ]
